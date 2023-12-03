a484c8c781b5cf5befdbdac40594c7eab2e3dc78
Artem <artem.belgorodkiy@yandex.ru>
2023-12-03 18:01:17 +0300

    filter xs products

--- src/Products.js
+++ src/Products.js
@@ -16,7 +16,15 @@ const Products = () => {
                     <button className='filter-button' onClick={() => setFilter(2)}>Диффузорры</button>
                     <button className='filter-button' onClick={() => setFilter(3)}>Свечи в бетоне</button>
                 </div>
-                <div className="col-12 flex-wrap d-flex justify-content-between flex-md-row flex-column col-md-10 align-items-center">
+                <div className="col-4 d-md-none flex-column">
+                    <select onChange={(e) => setFilter(e.target.value)} className='form-select select-product mb-2 p-1 shop-filter-text-md'>
+                    <option value={0} >Фильтр</option>
+                    <option value={1} >Свечи в стекле</option>
+                    <option value={2} >Диффузорры</option>
+                    <option value={3} >Свечи в бетоне</option>
+                    </select>
+                </div>
+                <div className="col-8 col-md-10 flex-wrap d-flex  flex-md-row flex-column col-md-10 align-items-center">
                     {goods.map(e =>
                          <div className= {classNames("card shop-card mb-4", {"d-none" : filter != e.filter && filter != 0} , {"d-block" : filter == e.filter || filter == 0})} >
                         <Link to={`product/${e.id}`}><img src={e.image} className="card-img-top img-shop" alt="..."/></Link>
